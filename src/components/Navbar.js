d90527d1599d96700926eddb48864f37ac2b0a7a
henrikpoa <henrik_poa@hotmail.com>
2023-05-25 17:51:33 -0300

    nav span uniasselvi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return <nav className={styles.navbar}>
         <NavLink to='/' className={styles.brand}>
-            Blog <span>Uniasslevi</span> ADS
+            <span>Uniasselvi - ADS</span>
         </NavLink>
         <ul className={styles.links_list}>
             <li>
